775a1caebb237f10862ad8923277b0c60058b8f4
ronnieoginga <oginga001ronney@gmail.com>
2019-02-12 12:57:03 +0300

    config update

---
+++ config.py
@@ -0,0 +1,35 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:001ronney@localhost/pitched_perfect'
+    #email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'ronnieogingaaminute-pitch' 
+    SENDER_EMAIL = 'oginga001ronney@gmail.com'
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class ProdConfig(Config):
+    DEBUG = True
+#     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+# class TestConfig(Config):
+#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringashool:1204@localhost/my_pitch_test'
+class DevConfig(Config):
+    DEBUG = True
+    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringaschool:1204@localhost/pitched_perfect'
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+# 'test':TestConfig
+}
